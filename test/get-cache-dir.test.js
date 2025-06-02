c02f0c133c12e52a2f4b63a9cf96759aa1cfc947
Mike Pirog <mike@lando.dev>
2025-06-02 15:14:07 -0400

    test up 3

--- test/get-cache-dir.test.js
+++ test/get-cache-dir.test.js
@@ -1,13 +1,21 @@
 import { describe, test, expect } from 'bun:test';
 import path from 'path';
+
 import getCacheDir from '../utils/get-cache-dir.js';
+import getPlatform from '../utils/get-platform.js';
 
 describe('get-cache-dir', () => {
   test('should compute cache directory for product', () => {
-    if (process.platform === 'darwin') {
+    if (getPlatform() === 'darwin') {
       expect(getCacheDir('foo')).toBe(path.join(process.env.HOME, 'Library', 'Caches', 'foo'));
     } else {
-      expect(getCacheDir('foo')).toBe(path.join(process.env.XDG_CACHE_HOME || path.join(process.env.HOME, '.cache'), 'foo'));
+      const XDG_CACHE_HOME = process.env.XDG_CACHE_HOME;
+      const home =
+        XDG_CACHE_HOME ??
+        process.env.HOME ??
+        (process.env.HOMEDRIVE && process.env.HOMEPATH && path.join(process.env.HOMEDRIVE, process.env.HOMEPATH)) ??
+        process.env.USERPROFILE;
+      expect(getCacheDir('foo')).toBe(path.join(home, '.cache'), 'foo');
     }
   });
 });
