1e724ff6ebd1f3d7e7dbd084aa0c1ec595fc6767
Mike Pirog <mike@lando.dev>
2025-06-02 15:44:43 -0400

    test up 9

--- test/parse-stdout-json.test.js
+++
@@ -1,9 +0,0 @@
-import { describe, test, expect } from 'bun:test';
-import parseStdOut from '../utils/parse-stdout-json.js';
-
-describe('parse-stdout-json', () => {
-  test('should run command and parse json output', () => {
-    const res = parseStdOut('echo "{\"a\":1}"'); // eslint-disable-line no-useless-escape
-    expect(res).toEqual({ a: 1 });
-  });
-});
